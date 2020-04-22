b325d5f1d6316b1c8e2ca51e07c3b15db02eb9ec
Paulo Henrique <paulohfcmendes@gmail.com>
2020-04-22 18:11:54 -0300

    feat/ UserControllers and SessionController

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   dialect: "postgres",
   host: "localhost",
   username: "postgres",
-  password: "cerradodeveloper",
+  password: "docker",
   database: "pagueakidb",
   define: {
     timestamps: true,
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,19 @@
 import { Router } from "express";
-
 import cors from "cors";
+
+import UserController from "./app/controllers/UserController";
+import SessionController from "./app/controllers/SessionController";
+import authMiddleware from "./app/middlewares/auth";
 
 const routes = new Router();
 
 routes.use(cors());
 
-routes.get("/", (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post("/sessions", SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.get("/users", UserController.index);
+routes.post("/users", UserController.store);
 
 export default routes;
