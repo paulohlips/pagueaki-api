f6cba8d29f27feb90ccdb04b5c427ebcae30fc62
Paulo Henrique <paulohfcmendes@gmail.com>
2020-05-18 22:04:16 -0300

    fix/

--- src/app/controllers/DownloadFileController.js
+++ src/app/controllers/DownloadFileController.js
@@ -6,7 +6,7 @@ class DownloadFileController {
     try {
       res.sendFile(path.resolve("stat", `${file}.pdf`));
     } catch (err) {
-      return res.json({ message: `Erro no servidor. ${err}` });
+      return res.json({ message: `Erro no servidor.${err}` });
     }
   }
 }
